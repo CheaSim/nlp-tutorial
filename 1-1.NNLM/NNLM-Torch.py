c2b49aa544dc96d19a64c51c7f54b7e230e9c953
CheaSim <31753427+CheaSim@users.noreply.github.com>
2020-07-25 18:18:51 +0800

    update to a 船新版本
    added pytorch-lightning

--- 1-1.NNLM/NNLM-Torch.py
+++ 1-1.NNLM/NNLM-Torch.py
@@ -1,18 +1,29 @@
 # code by Tae Hwan Jung @graykode
-import numpy as np
-import torch
+# modified by Chea Sim @CheaSim
+import numpy as np #导入numpy包
+
+import pytorch_lightning as pl
+import torch 
 import torch.nn as nn
-import torch.optim as optim
-from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.optim as optim # torch的优化器 有SGD Adam
+from torch.utils.data import Dataset, DataLoader
+import tensorboard
+
 
 dtype = torch.FloatTensor
-
 sentences = [ "i like dog", "i love coffee", "i hate milk"]
 
-word_list = " ".join(sentences).split()
-word_list = list(set(word_list))
-word_dict = {w: i for i, w in enumerate(word_list)}
-number_dict = {i: w for i, w in enumerate(word_list)}
+
+word_list = " ".join(sentences).split() # 为了提取每个句子中的词语，我们先将所有句子用" "分开之后
+#在用split()将所有单词分开获得一个word_list
+word_list = list(set(word_list)) #去重
+
+#{} 是词典的意思 这两个一对一产生类似于map的功能
+# word_ditc[word] = idx
+# number_dict[idx] = word
+word_dict = {w: i for (i, w) in enumerate(word_list)}
+number_dict = {i: w for (i, w) in enumerate(word_list)}
 n_class = len(word_dict) # number of Vocabulary
 
 # NNLM Parameter
@@ -20,63 +31,119 @@ n_step = 2 # n-1 in paper
 n_hidden = 2 # h in paper
 m = 2 # m in paper
 
-def make_batch(sentences):
-    input_batch = []
-    target_batch = []
+class MyDataset(Dataset):
+    def __init__(self, sentences):
+        super().__init__()
+        self.x, self.y = self.make_batch(sentences)
 
-    for sen in sentences:
-        word = sen.split()
-        input = [word_dict[n] for n in word[:-1]]
-        target = word_dict[word[-1]]
+    def __len__(self):
+        return len(self.x)
 
-        input_batch.append(input)
-        target_batch.append(target)
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
+        
+    def make_batch(self, sentences):
+        input_batch = []
+        target_batch = []
 
-    return input_batch, target_batch
+        for sen in sentences:
+            # split the words into a list
+            word = sen.split()
+            # insert all wordds into input except the last one as the last one is the target
+            input = [word_dict[n] for n in word[:-1]]
+            # target is the id of the last word
+            target = word_dict[word[-1]]
+
+            # append a list to a list. conxxx to 
+            # input_batch = [input, input, input]
+            input_batch.append(input)
+            target_batch.append(target)
+
+        return torch.tensor(input_batch, dtype=int), torch.tensor(target_batch, dtype=int)
+
 
 # Model
-class NNLM(nn.Module):
-    def __init__(self):
-        super(NNLM, self).__init__()
-        self.C = nn.Embedding(n_class, m)
-        self.H = nn.Parameter(torch.randn(n_step * m, n_hidden).type(dtype))
-        self.W = nn.Parameter(torch.randn(n_step * m, n_class).type(dtype))
-        self.d = nn.Parameter(torch.randn(n_hidden).type(dtype))
-        self.U = nn.Parameter(torch.randn(n_hidden, n_class).type(dtype))
-        self.b = nn.Parameter(torch.randn(n_class).type(dtype))
+class NNLM(pl.LightningModule):
+    def __init__(self, sentences):
+        super().__init__()
+        # Embedding 是一个函数 将一个word转化为高维向量的形式。
+        # m = 2 表示输入是2维的input
+        self.C = nn.Embedding(n_class, m) 
+        self.l1 = nn.Linear(m*n_step, n_hidden)
+        self.l3 = nn.Linear(m*n_step, n_class,bias = False)
+        # self.H = nn.Parameter(torch.randn(n_step * m, n_hidden).type(dtype))
+        # self.W = nn.Parameter(torch.randn(n_step * m, n_class).type(dtype))
+        # self.d = nn.Parameter(torch.randn(n_hidden).type(dtype))
+        self.l2 = nn.Linear(n_hidden, n_class)
+        # self.U = nn.Parameter(torch.randn(n_hidden, n_class).type(dtype))
+        # self.b = nn.Parameter(torch.randn(n_class).type(dtype))
+        self.sentences = sentences
 
     def forward(self, X):
+        # 第一步先编码
         X = self.C(X)
         X = X.view(-1, n_step * m) # [batch_size, n_step * n_class]
-        tanh = torch.tanh(self.d + torch.mm(X, self.H)) # [batch_size, n_hidden]
-        output = self.b + torch.mm(X, self.W) + torch.mm(tanh, self.U) # [batch_size, n_class]
+        # tanh = torch.tanh(self.d + torch.mm(X, self.H)) # [batch_size, n_hidden]
+        tanh = torch.tanh(self.l1(X))
+        # output = self.b + torch.mm(X, self.W) + torch.mm(tanh, self.U) # [batch_size, n_class]
+        output = self.l3(X) + self.l2(tanh)
         return output
 
-model = NNLM()
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        tensorboard_logs = {'train_loss': loss}
+        return {'loss': loss, 'log': tensorboard_logs}
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    def train_dataloader(self):
+        train_dataset = MyDataset(self.sentences)
+        train_loader = DataLoader(train_dataset,batch_size=3, shuffle=True)
+        return train_loader
 
-input_batch, target_batch = make_batch(sentences)
-input_batch = Variable(torch.LongTensor(input_batch))
-target_batch = Variable(torch.LongTensor(target_batch))
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=0.001)
+    
+    def SHOW_ME(self):
+        input_batch = []
+        target_batch = []
+
+        for sen in sentences:
+            # split the words into a list
+            word = sen.split()
+            # insert all wordds into input except the last one as the last one is the target
+            input = [word_dict[n] for n in word[:-1]]
+            # target is the id of the last word
+            target = word_dict[word[-1]]
+
+            # append a list to a list. conxxx to 
+            # input_batch = [input, input, input]
+            input_batch.append(input)
+            target_batch.append(target)
+
+        input_batch = torch.tensor(input_batch, dtype=int)
+        predict = self(input_batch).data.max(1, keepdim=True)[1]
+        print(predict.squeeze())
+
+        print(predict)
+        # Test
+        print([sen.split()[:2] for sen in sentences], '->', [number_dict[n.item()] for n in predict.squeeze()])
+
+        
+
+
 
 # Training
-for epoch in range(5000):
 
-    optimizer.zero_grad()
-    output = model(input_batch)
+def main():
+    model = NNLM(sentences = sentences)
+    trainer = pl.Trainer(max_epochs=5000, gpus=1)
+    trainer.fit(model)
+    model.SHOW_ME()
 
-    # output : [batch_size, n_class], target_batch : [batch_size] (LongTensor, not one-hot)
-    loss = criterion(output, target_batch)
-    if (epoch + 1)%1000 == 0:
-        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+   
 
-    loss.backward()
-    optimizer.step()
-
-# Predict
-predict = model(input_batch).data.max(1, keepdim=True)[1]
-
-# Test
-print([sen.split()[:2] for sen in sentences], '->', [number_dict[n.item()] for n in predict.squeeze()])
+# model(input_batch) 获得概率函数， 之后取其中最大的。
+if __name__ == '__main__':
+    main()
